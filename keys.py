29fef83a0623233c2ecbd38590560f9366fab62b
Brian Mitchell <bman4789@gmail.com>
2015-03-13 23:45:55 -0500

    added keys.py file, now loops repeatedly and will tweet four times a day

---
+++ keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#keys.py
+
+keys = dict(
+	consumer_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+	consumer_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+	access_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+	access_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+)
--- weatherBot.py
+++ weatherBot.py
@@ -5,19 +5,19 @@
 #Copyright 2015 Brian Mitchell under the MIT license
 #See the GitHub repository: https://github.com/bman4789/weatherBot
 
+from datetime import datetime
+import time
 import tweepy, urllib2, urllib, json
+from keys import keys
 
 #Contants
-CONSUMER_KEY = '...' #Twitter app consumer key
-CONSUMER_SECRET = '...' #twitter app consumer secret
-ACCESS_KEY = '...' #twitter app user access key
-ACCESS_SECRET = '...' #twitter app user access secret
-WOEID = '2454256' #Yahoo location ID used for the weather location
+WOEID = '2454256' #Yahoo location ID
+LOOP_SLEEP_TIME = 30 #number of seconds that go by between loops
 
-
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
+CONSUMER_KEY = keys['consumer_key']
+CONSUMER_SECRET = keys['consumer_secret']
+ACCESS_KEY = keys['access_key']
+ACCESS_SECRET = keys['access_secret']
 
 #Grr unicode support in Python 2
 deg = "º"
@@ -30,8 +30,7 @@ def getWeather():
     yresult = urllib2.urlopen(yql_url).read()
     return json.loads(yresult)
     
-def makeTweet():
-    ydata = getWeather()
+def makeTweet(ydata):
     windchill = int(ydata['query']['results']['channel']['wind']['chill'])
     windspeed = int(ydata['query']['results']['channel']['wind']['speed'])
     humidity = int(ydata['query']['results']['channel']['atmosphere']['humidity'])
@@ -57,6 +56,8 @@ def makeTweet():
         return "Drizzlin' yo."
     elif (humidity == 100 and (code != 10 or code != 11 or code != 12 or code != 37 or code != 38 or code != 39 or code != 40 or code != 45 or code != 47)):
         return "Damn, it's 100% humid. Glad I'm not a toilet so water doesn't condense on me."
+    elif (humidity < 5):
+        return "It's dry as fuck. Fucking " + str(humidity) + "% humid right now."
     elif (temp <= -20):
         return "It's fucking " + str(temp) + deg + "F. Too fucking cold."
     elif (temp >= 100):
@@ -67,6 +68,48 @@ def makeTweet():
         return "Someone fucked up, apparently the current condition is \"not available\" http://www.reactiongifs.com/wp-content/uploads/2013/08/air-quotes.gif"
     else:
         return "The weather is fucking boring. " + str(temp) + deg + "F and " + condition + "."
-        
-# api.update_status(status=makeTweet())
-print makeTweet()
+
+def doTweet():
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
+    ydata = getWeather()
+    
+    content = makeTweet(ydata)
+    latitude = ydata['query']['results']['channel']['item']['lat']
+    longitude = ydata['query']['results']['channel']['item']['long']
+    print content
+    api.update_status(status=content,lat=latitude,long=longitude)
+
+count = 1
+while(True):
+    print "loop", count
+    now = datetime.now()
+    time1 = now.replace(hour=7, minute=0, second=0, microsecond=0) #the time of the first tweet to go out
+    time1late = time1.replace(second=LOOP_SLEEP_TIME) #LOOP_SLEEP_TIME after so the tweet won't be triggered every time
+    time2 = now.replace(hour=12, minute=0, second=0, microsecond=0)
+    time2late = time2.replace(second=LOOP_SLEEP_TIME)
+    time3 = now.replace(hour=15, minute=30, second=0, microsecond=0)
+    time3late = time3.replace(second=LOOP_SLEEP_TIME)
+    time4 = now.replace(hour=20, minute=0, second=0, microsecond=0)
+    time4late = time4.replace(second=LOOP_SLEEP_TIME)
+    
+    if (now  > time4 and now < time4late):
+        print "Time:", datetime.now().time().strftime("%H:%M:%S")
+        print "time4"
+        doTweet()
+    elif (now > time3 and now < time3late):
+        print "Time:", datetime.now().time().strftime("%H:%M:%S")
+        print "time3"
+        doTweet()
+    elif (now > time2 and now < time2late):
+        print "Time:", datetime.now().time().strftime("%H:%M:%S")
+        print "time2"
+        doTweet()
+    elif (now > time1 and now < time1late):
+        print "Time:", datetime.now().time().strftime("%H:%M:%S")
+        print "time1"
+        doTweet()
+    
+    time.sleep(LOOP_SLEEP_TIME)
+    count = count + 1
