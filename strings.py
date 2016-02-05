fcb442abce50a0819fe440332f19c54625d7ab21
DjGusaas <gusaa004@morris.umn.edu>
2016-02-05 04:40:27 +0000

    Fixed missing ')'

--- strings.py
+++ strings.py
@@ -107,7 +107,7 @@ def get_special_condition(weather_data):
         text = 'It\'s ' + weather_data['temp_and_unit'] + '. Too cold.'
         return 'cold', text
     elif (weather_data['units']['temperature'] == 'F' and weather_data['temp'] >= 110) or \
-            (weather_data['units']['temperature'] == 'C' and 43 <=  weather_data['temp']:
+            (weather_data['units']['temperature'] == 'C' and 43 <=  weather_data['temp']):
         text = 'Wowowowowowowowow, it\'s ' + weather_data['temp_and_unit'] + '. I need some A/C ASAP.'
         return 'super-hot', text
     elif (weather_data['units']['temperature'] == 'F' and weather_data['temp'] >= 100) or \
